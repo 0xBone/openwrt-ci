9c825652c3fed797e6123c7097bac42604a960aa
Shibone <65435898+0xBone@users.noreply.github.com>
2026-04-17 19:39:34 +0800

    Update libwrt.sh to clone new packages
    Added commands to clone and set up additional packages.

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,15 @@
+echo "lib wrt"
+
 rm -rf package/emortal/luci-app-athena-led
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+
+
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+
+./scripts/feeds update -a
+./scripts/feeds install -a
